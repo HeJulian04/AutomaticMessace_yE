dd170886101ae6cb9195c7ea521398ea5eca18a9
HeJulian04 <julian.henz@bbzbl-it.ch>
2021-09-30 11:08:29 +0200

    hoffentlich das letzte mal

--- EmailMessage.py
+++ EmailMessage.py
@@ -9,11 +9,12 @@ import calendar
 logFile = open("logs.txt", "a")
 
 employees = ["alina.wuethrich", "beat.haenger", "benjamin.brodwolf", "benji.oser", "carl.kuesters", "christina.dasilva", "dominik.erni", "gabriel.brodmann", "gerd-emmanuel.nandzik" ,"joshua.brehm", "joeal.muller", "julian.henz", "marc.baur", "mario.hammel", "martin.luepold", "meredit.sommer", "michael.hunziker", "nadia.kramer", "niki.stohler", "pascal.andermatt", "sibylle.tanner", "simon.fankhauser", "simon.spruengli"]
-port = 465  # For SSL
+port = 465  # For SSL,
 smtp_server = "smtp.gmail.com"
 sender_email = "youengineering.test@gmail.com"  # Enter your address
 password = "123456yE"
 message = """\
+Subject: Monatsende Hakuna Erinnnerung
 Monatsende
 
 Denkt bitte daran bis heute Abend eure Stunden in Hakuna vollständig zu erfassen. 
@@ -21,23 +22,30 @@ Merci vielmals und allne ä hübsche Tag :D  Sibylle
 """
 
 
-def message_send_function():
-    today = datetime.date.today()
 
+today = datetime.date.today()
+
+def check_last_day():
     last = today.replace(day=calendar.monthrange(today.year, today.month)[1])
+    if calendar.day_name[last.weekday()] == "Saturday":
+        last = last + timedelta(days=-1)
+        return last
+    elif calendar.day_name[last.weekday()] == "Sunday":
+        last = last + timedelta(days=-2)
+        return last
+    else:
+        return last
 
-    lastBusinessDay = last - timedelta(days=1 + last.weekday() - 5)
+def message_send_function():
 
-    if lastBusinessDay == today:
+    if today == check_last_day():
 
-        logFile.write(lastBusinessDay.strftime("%d-%b-%Y"))
-        logFile.write("\n")
         for employee in employees:
             receiver_email = employee + "@youengineering.ch"
             context = ssl.create_default_context()
             with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
                 server.login(sender_email, password)
-                server.sendmail(sender_email, receiver_email, message)
+                server.sendmail(sender_email, receiver_email, message.encode('utf-8', 'ignore'))
 
             logFile.write("Email successfully to " + receiver_email + " send")
             logFile.write("\n")
